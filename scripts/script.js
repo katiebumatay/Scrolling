a8e0551fbfc7e5199c551995bf6d4dcc430ed643
katiebumatay <31480009+katiebumatay@users.noreply.github.com>
2018-10-20 18:01:28 -0500

    letters move vertically only

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 
-
+(function () {
+    "use strict";
+    // Cycle over each .shuffledv HTMLElement
+    $(".skrollr-container").each(function () {
+        // Remove all divs within, store in $d
+        var $d = $(this).find(".small-container").remove();
+        // Sort $d randomnly
+        $d.sort(function () { return Math.floor(Math.random() * $d.length); });
+        // Append divs within $d to .shuffledv again
+        $d.appendTo(this);
+    });
+}());
 
 
 
